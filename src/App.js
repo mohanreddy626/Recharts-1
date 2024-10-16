16a5bc1a5ff8f13078a66f2e033911dca3dd0ae1
mohanreddy626 <mohanreddie626@gmail.com>
2024-10-16 13:28:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Recharts from './Recharts'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Recharts:-</h1>
+      <Recharts />
+    </div>
+  )
+}
